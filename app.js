289d0ea65686f09f684bb08d8d676d702c2106f6
vaidotasp <vaidotasp@gmail.com>
2016-09-16 17:48:59 -0400

    sync location and api request

--- app.js
+++ app.js
@@ -1,68 +1,66 @@
+//Global Initializer
 window.onload = function() {
+var timeNow =  new Date().toLocaleTimeString();
+var url;
+locator();
+
 
 function locator(){
   var result = document.getElementById('main');
 
-  navigator.geolocation.getCurrentPosition(success/* ,error*/);
+  navigator.geolocation.getCurrentPosition(success ,error);
 
   function success(position){   
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     result.innerHTML = 'Latitude is' + lat + 'Longitude is' + long;
-    console.log(url);  
     url = 'http://api.openweathermap.org/data/2.5/weather?' + 'lat=' + lat + '&lon=' + long + '&units=metric&APPID=f72629dad5e8bf650b1a6290c7ea5374';
-    console.log('post inject', url);  
+    console.log('post inject url: ', url);
+    handler();
+
   };
-  // //Location is not available
-  // function error(){
-  //   result.innerHTML = 'Location unavailable';
-  // };
+  //Location is not available
+  function error(){
+    console.log("Unavailable");
+    var visible = document.querySelector('#locUnav');
+    visible.style.visibility = "visible";
+  };
   
-  //Initialize Geolocation API, pass success and error functions
 }
 
-  locator();
-
+  
   //AJAX Call to Weather API using location
-  
-  var btn = document.getElementById('btn');
-  var url='http://api.openweathermap.org/data/2.5/weather?lat=38.928&lon=-77.02&units=metric&APPID=f72629dad5e8bf650b1a6290c7ea5374'
-  
+    
   var request = new XMLHttpRequest();
-
   request.onreadystatechange = function(){
     if (request.readyState === 4) {
       if (request.status === 200){
-        console.log('OKAY');
+        console.log('AJAX Call Success');
         console.log('Raw JSON response: ',request.responseText);
         var result = JSON.parse(request.responseText);
         console.log(result);
-        //Things to extract: Name/City, Temp, Hi, Low -- anything else?
         var city = result.name;
         var temp = result.main['temp'].toFixed();
         var tempHigh = result.main['temp_max'].toFixed();
         var tempLow = result.main['temp_min'].toFixed();
         var condition = result.weather[0]['main'];
        
-        document.getElementById("temp").innerHTML = temp + "F";
-        document.getElementById("city").innerHTML = city;
-        document.getElementById("hilow").innerHTML = 'HI:' + tempHigh +' ' + 'LO:' +tempLow;
-        document.getElementById("con").innerHTML = condition;
-
-
-
+        document.getElementById('temp').innerHTML = temp + 'F';
+        document.getElementById('city').innerHTML = city;
+        document.getElementById('time').innerHTML = timeNow;
+        document.getElementById('hilow').innerHTML = 'HI:' + tempHigh +' ' + 'LO:' +tempLow;
+        document.getElementById('con').innerHTML = condition;
 
       } else {
-        console.log('Not Okay');
+        console.log('AJAX Call Failed');
       }
     }
   };
-  //event listener
-  btn.addEventListener('click', function handler(){
+  //ajax initializer
+  function handler(){
     request.open('GET',url);
-
     request.send();
-  });
+  };
 
 
 };
