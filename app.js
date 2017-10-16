9b9dadffb42103a1110dc7e96a95d2bf4097e31e
vaidotasp <vaidotasp@gmail.com>
2017-10-15 21:09:37 -0400

    file structure

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(express.static('/'))
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  res.send('index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3000 || process.env.PORT)
