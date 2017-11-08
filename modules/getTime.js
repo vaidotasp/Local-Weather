57786b2344d50d49f7e32ea91c5dc8d02e4800dc
vaidotasp <vaidotasp@gmail.com>
2017-11-08 06:17:32 -0500

    first version of promise implementation

--- modules/getTime.js
+++ modules/getTime.js
@@ -2,7 +2,6 @@ const moment = require('moment')
 moment().format()
 
 //current time in string format: Wednesday, 18:00
-
 let getTime = function() {
   let now = moment()
   now = now.format('dddd, HH:mm')
--- routes.js
+++ routes.js
@@ -5,48 +5,54 @@ const getWeather = require('./modules/getWeather')
 const getTime = require('./modules/getTime')
 
 router.get('/', function(req, res, next) {
-  // getCityName(23.084107, -82.385197)
-  //   .then(function(city) {})
-  //   .catch(err => console.log(err))
-  // getWeather(23.084107, -82.385197)
-  //   .then(function(data) {
-  //     return data // no idea why I need then chaining here to access data?
-  //   })
-  //   .then(function(all) {})
-  //   .catch(err => console.log(err))
   res.render('index')
 })
 
 router.post('/', function(req, res, next) {
   let lat = req.body.lat
   let long = req.body.long
-  let locationInfo = {}
+  // let locationInfo = {}
 
-  getCityName(lat, long)
-    .then(function(city) {
-      return city
-    })
-    .then(function(cityName) {
-      console.log(`City Name is: ${cityName}`)
-      locationInfo.cityName = cityName
-    })
-    .catch(err => console.log(err))
-  getWeather(lat, long)
-    .then(function(data) {
-      return data // no idea why I need then chaining here to access data?
-    })
-    .then(function(all) {
-      console.log(all)
-      let timeNow = getTime()
-      locationInfo.time = timeNow
-      locationInfo['temp'] = all.temp
-      locationInfo.humidity = all.humidity
-      locationInfo.icon = all.icon
-      locationInfo.summary = all.summary
-      locationInfo.forecast = all.forecast
-      res.json(locationInfo)
-    })
-    .catch(err => console.log(err))
+  //getCityName(lat, long)
+  // .then(function(city) {
+  //   return city
+  // })
+  // .then(function(cityName) {
+  //   console.log(`City Name is: ${cityName}`)
+  //   locationInfo.cityName = cityName
+  //   return cityName
+  // })
+  // .catch(err => console.log(err))
+  //getWeather(lat, long)
+  // .then(function(data) {
+  //   return data // no idea why I need then chaining here to access data?
+  // })
+  // .then(function(all) {
+  //   console.log(all)
+  //   let timeNow = getTime()
+  //   locationInfo.time = timeNow
+  //   locationInfo['temp'] = all.temp
+  //   locationInfo.humidity = all.humidity
+  //   locationInfo.icon = all.icon
+  //   locationInfo.summary = all.summary
+  //   locationInfo.forecast = all.forecast
+  //   res.json(locationInfo)
+  // })
+  // .catch(err => console.log(err))
+
+  Promise.all([getCityName(lat, long), getWeather(lat, long)]).then(values => {
+    console.log(values)
+    let locationInfo = {}
+    let timeNow = getTime()
+    locationInfo.time = timeNow
+    locationInfo.cityName = values[0]
+    locationInfo['temp'] = values[1].temp
+    locationInfo.humidity = values[1].humidity
+    locationInfo.icon = values[1].icon
+    locationInfo.summary = values[1].summary
+    locationInfo.forecast = values[1].forecast
+    res.json(locationInfo)
+  })
 })
 
 module.exports = router
