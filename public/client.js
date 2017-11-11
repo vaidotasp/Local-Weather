bd754d1aadb158b1315289d0e02e13442a6fdb03
vaidotasp <vaidotasp@gmail.com>
2017-11-11 08:26:00 -0500

    overall cleanup

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 let locator = new Promise(function(resolve, reject) {
   console.log('Fetching location...')
-
+  //Hide all elements except "fetching Location?"
   navigator.geolocation.getCurrentPosition(success, error)
   function error(position) {
     console.log('Location unavailable')
@@ -40,7 +40,6 @@ locator.then(({ lat, long }) => {
     .then(status)
     .then(json)
     .then(function(data) {
-      console.log('data output goes here: ', data)
       let temperature = data.temp
       temperature = temperature.toFixed() + '°C'
       document.getElementById('temp').innerHTML = temperature
@@ -49,7 +48,6 @@ locator.then(({ lat, long }) => {
       document.getElementById('time').innerHTML = data.time
       document.getElementById('location').innerHTML = data.cityName
       weatherIcon(data.icon)
-      console.log(`Icon is this one: ${data.icon}`)
     })
     .catch(function(error) {
       console.log('request failed: ', error)
@@ -64,12 +62,9 @@ function tempChange() {
   let contentElem = document.getElementById('temp')
   let tempIndicator = contentElem.innerText[contentElem.innerText.length - 1]
   let newTemp = contentElem.innerText
-  console.log(contentElem.innerText.length)
   if (contentElem.innerText.length === 3) {
     newTemp = '0' + contentElem.innerText
-    console.log(newTemp)
   }
-  console.log(tempIndicator)
   if (tempIndicator === 'C') {
     let convertedContentElem = (newTemp.slice(0, 2) * 9 / 5 + 32).toFixed()
     contentElem.innerText = convertedContentElem + '°F'
@@ -88,7 +83,6 @@ const themeToggleBtn = document.getElementById('themeBtn')
 themeToggleBtn.addEventListener('click', toggleTheme)
 
 function toggleTheme() {
-  console.log('toggle btn clicked')
   //check for which theme is loaded first
   let h1 = document.querySelector('.h1')
   let body = document.querySelector('.body')
@@ -96,16 +90,13 @@ function toggleTheme() {
   let mainDivStyle = window
     .getComputedStyle(mainDiv, null)
     .getPropertyValue('background-color')
-  console.log(mainDivStyle)
   if (mainDivStyle === 'rgb(51, 51, 51)') {
-    console.log('we are in dark theme territory')
     //change it to light theme
     mainDiv.style.backgroundColor = '#2D9CDB'
     h1.style.color = '#2D9CDB'
     body.style.backgroundColor = '#FFFFFF'
   }
   if (mainDivStyle === 'rgb(45, 156, 219)') {
-    console.log('we are in light theme territory')
     body.style.backgroundColor = '#d8d8d8'
     mainDiv.style.backgroundColor = '#333333'
     h1.style.color = '#333333'
@@ -134,81 +125,3 @@ function weatherIcon(icon) {
       break
   }
 }
-
-//     function getWeather() {
-//       $.ajax({
-//         url: url,
-//         dataType: 'jsonp',
-//         success: function(weatherInfo) {
-//           var temp = weatherInfo.currently['temperature'].toFixed()
-//           var condition = weatherInfo.currently['icon']
-//           var summary = weatherInfo.currently['summary']
-//           condition = condition.charAt(0).toUpperCase() + condition.slice(1)
-//           document.getElementById('temp').innerHTML = temp
-//           document.getElementById('time').innerHTML = timeNow
-//           document.getElementById('con').innerHTML = summary
-//           switch (condition) {
-//             case 'clear-day' || 'clear-night':
-//               document.getElementById('image').src = 'img/clear.png'
-//               break
-//             case 'cloudy' || 'partly-cloudy-day' || 'partly-cloudy-night':
-//               document.getElementById('image').src = 'img/cloudy.png'
-//               break
-//             case 'rain':
-//               document.getElementById('image').src = 'img/drizzle.png'
-//               break
-//             case 'snow' || 'sleet':
-//               document.getElementById('image').src = 'img/snow.png'
-//               break
-//             case 'Thunderstorm':
-//               document.getElementById('image').src = 'img/storm.png'
-//               break
-//             default:
-//               document.getElementById('image').src = 'img/cloudy.png'
-//               break
-//           }
-//         }
-//       })
-//     }
-//   }
-//   //Location is not available
-//   function error() {
-//     var visible = document.querySelector('#locUnav')
-//     visible.style.visibility = 'visible'
-//     var cVisibility = document.querySelector('#cel')
-//     cVisibility.style.visibility = 'hidden'
-//     var fVisibility = document.querySelector('#far')
-//     fVisibility.style.visibility = 'hidden'
-//   }
-// }
-
-// //toggles the temp and converts it
-// const cTemp = document.getElementById('cel')
-// const fTemp = document.getElementById('far')
-// cTemp.addEventListener('click', cTempConversion)
-// fTemp.addEventListener('click', fTempConversion)
-
-// function cTempConversion() {
-//   document.getElementById('temp').innerHTML = ((document.getElementById('temp')
-//     .innerHTML -
-//     32) *
-//     5 /
-//     9
-//   ).toFixed()
-//   cTemp.removeEventListener('click', cTempConversion)
-//   fTemp.addEventListener('click', fTempConversion)
-// }
-
-// function fTempConversion() {
-//   const tempToggle = document.querySelector('#cel')
-//   tempToggle.style['pointer-events'] = 'auto'
-//   document.getElementById('temp').innerHTML = (parseInt(
-//     document.getElementById('temp').innerHTML
-//   ) *
-//     9 /
-//     5 +
-//     32
-//   ).toFixed()
-//   cTemp.addEventListener('click', cTempConversion)
-//   fTemp.removeEventListener('click', fTempConversion)
-// }
--- routes.js
+++ routes.js
@@ -11,34 +11,6 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   let lat = req.body.lat
   let long = req.body.long
-  // let locationInfo = {}
-
-  //getCityName(lat, long)
-  // .then(function(city) {
-  //   return city
-  // })
-  // .then(function(cityName) {
-  //   console.log(`City Name is: ${cityName}`)
-  //   locationInfo.cityName = cityName
-  //   return cityName
-  // })
-  // .catch(err => console.log(err))
-  //getWeather(lat, long)
-  // .then(function(data) {
-  //   return data // no idea why I need then chaining here to access data?
-  // })
-  // .then(function(all) {
-  //   console.log(all)
-  //   let timeNow = getTime()
-  //   locationInfo.time = timeNow
-  //   locationInfo['temp'] = all.temp
-  //   locationInfo.humidity = all.humidity
-  //   locationInfo.icon = all.icon
-  //   locationInfo.summary = all.summary
-  //   locationInfo.forecast = all.forecast
-  //   res.json(locationInfo)
-  // })
-  // .catch(err => console.log(err))
 
   Promise.all([getCityName(lat, long), getWeather(lat, long)]).then(values => {
     console.log(values)
